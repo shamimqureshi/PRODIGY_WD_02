55cb1b5d890dffb49d76a6449f2f1d670d7d0a1b
shamimqureshi <152699341+shamimqureshi@users.noreply.github.com>
2024-06-16 01:34:53 +0530

    STOPWATCH

---
+++ script.js
@@ -0,0 +1,41 @@
+let[seconds, minutes, hours]=[0,0,0];
+let displayTime=document.getElementById("displayTime");
+let timer =null;
+
+function Stopwatch(){
+    seconds++;
+    if(seconds == 60){
+       seconds = 0; 
+       minutes++;
+       if(minutes == 60){
+        minutes = 0;
+        hours++;
+
+       }
+    }
+
+     let h = hours < 10 ? "0" + hours :hours;
+     let m = minutes < 10 ? "0" + minutes :minutes;
+     let s = seconds < 10 ? "0" + seconds :seconds;
+
+
+
+    displayTime.innerHTML = h +":"+ m +":"+ s;
+}
+function watchstart(){   
+              if(timer!==null)
+              {
+                clearInterval(timer);
+              }
+timer =setInterval(Stopwatch,1000);
+}
+
+function watchstop(){
+clearInterval(timer);
+}
+
+function watchreset(){
+clearInterval(timer);
+[seconds, minutes, hours] = [0,0,0];
+displayTime.innerHTML= "00:00:00";
+}
